43ff05bbbb78163d2e3eaa18972ca61b0a763a30
Timo Ewalds <timo@ewalds.ca>
2026-02-11 03:13:20 +0000

    github user/branch

--- ct/kiwix.sh
+++ ct/kiwix.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+export GITHUB_USER="tewalds"
+export GITHUB_BRANCH="feature/kiwix"
+
 #source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 source <(curl -fsSL https://raw.githubusercontent.com/tewalds/ProxmoxVE/feature/kiwix/misc/build.func)
 # Copyright (c) 2021-2026 community-scripts ORG
